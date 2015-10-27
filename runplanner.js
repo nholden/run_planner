e67ce12add21d9f501f47371f5c56c155770e17b
Nick Holden <nholden@bu.edu>
2015-10-26 20:26:05 -0400

    Generalize currentTemp function to create currentWeather function

--- runplanner.js
+++ runplanner.js
@@ -15,30 +15,37 @@ function buildLocationForm () {
   locationSubmit.addEventListener("click", function(event) {
     event.preventDefault();
     try {
-      var temp = currentTemp(zipCodeInput.value);
-      tempDiv.textContent = "It is currently " + temp + " degrees in zip code " + zipCodeInput.value + "."; 
+      var weather = currentWeather(zipCodeInput.value);
+      weatherDiv.innerHTML = "Weather in zip code " + zipCodeInput.value +
+                             "<br>Temperature: " + weather.temp + "&deg;F" +
+                             "<br>Conditions: " + weather.cond +
+                             "<br>Wind speed: " + weather.wind + " mph";
     } catch(err) {
-      tempDiv.textContent = err;
+      weatherDiv.textContent = err;
     }
   });
 
-  var tempDiv = document.createElement("div");
-  document.body.appendChild(tempDiv);
+  var weatherDiv = document.createElement("div");
+  document.body.appendChild(weatherDiv);
 }
 
-// given a US zip code, returns the current temperature in degrees fahrenheit
-function currentTemp(zipCode) {
+// given a US zip code, returns object with temp, cond, and wind properties
+function currentWeather(zipCode) {
   var req = new XMLHttpRequest();
-  req.open("GET", "http://api.openweathermap.org/data/2.5/weather?zip=" 
-           + zipCode + ",us&APPID=e3f7ff8b1714bf3efa20664e097b5387", false);
+  req.open("GET", "http://api.openweathermap.org/data/2.5/weather?zip=" + zipCode +
+           ",us&units=imperial&APPID=e3f7ff8b1714bf3efa20664e097b5387", false);
   req.send(null);
   if (req.status == 200) {
-    var weatherData = JSON.parse(req.responseText);
-    if (weatherData.cod == 200) {
-      var temp = Math.round((weatherData.main.temp - 273.15)*1.8+32);
-      return temp;
+    var data = JSON.parse(req.responseText);
+    if (data.cod == 200) {
+      var weather = {
+        temp: Math.round(data.main.temp),
+        cond: data.weather[0].main,
+        wind: data.wind.speed
+      };
+      return weather;
     } else {
-      throw weatherData.message;
+      throw data.message;
     }
   } else {
     throw req.statusText;
